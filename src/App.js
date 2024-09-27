606e62ada8060022523c47aff797bd777915156c
AkhilKrish1224 <akhilkrishnan1224@gmail.com>
2024-09-27 17:45:46 +0530

    Added DevTools and react query refetch

--- src/App.js
+++ src/App.js
@@ -3,20 +3,33 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-  const { data, error, isLoading } = useQuery({
-    queryKey: ["test"],
+  const priceQuery = useQuery({
+    queryKey: ["price"],
     queryFn: () => {
       return axios(
         "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
       );
     },
+    refetchInterval: 1000 * 20,
+  });
+
+  const customerQuery = useQuery({
+    queryKey: ["customers"],
+    queryFn: () => {
+      return axios("http://localhost:3000/api/customers");
+    },
+    refetchInterval: 1000 * 20,
   });
 
   return (
     <div className="App">
-      {error ? <p>UH oh! Error...</p> : null}
-      {isLoading ? <p>Loading...</p> : null}
-      {data?.data?.bitcoin?.usd ? data.data.bitcoin.usd : <p>Null</p>}
+      {priceQuery?.error ? <p>UH oh! Error...</p> : null}
+      {priceQuery?.isLoading ? <p>Loading...</p> : null}
+      {priceQuery?.data?.data?.bitcoin?.usd ? (
+        priceQuery.data.data.bitcoin.usd
+      ) : (
+        <p>Null</p>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,7 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
