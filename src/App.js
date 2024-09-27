f1b64647c32ab2eed0d2ce9a858eda59779b1ca7
AkhilKrish1224 <akhilkrishnan1224@gmail.com>
2024-09-27 15:30:25 +0530

    Added react-query

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  return <div className="App">Hello</div>;
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["test"],
+    queryFn: () => {
+      return axios(
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+      );
+    },
+  });
+
+  return (
+    <div className="App">
+      {error ? <p>UH oh! Error...</p> : null}
+      {isLoading ? <p>Loading...</p> : null}
+      {data?.data?.bitcoin?.usd ? data.data.bitcoin.usd : <p>Null</p>}
+    </div>
+  );
 }
 
 export default App;
